08d7f256b7ca607e5153cf6a35f97681c1190fe6
ProfitDev <szhylyuk84@gmail.com>
2021-02-22 17:56:30 +0200

    js code update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
-// (function() {
-//     const burger = document.querySelector('.burger-btn');
-//     burger.addEventListener('click', () => {
-//         burger.classList.toggle('burger_active');
-//     });
-// }());
 
-$(document).ready(function () {
-    $('.header-burger').click(function (event) {
-        $('.header-burger,.header-menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
+// java script code
+
+document.querySelector('.header-burger,.header-menu').addEventListener('click', toggleClass);
+
+function toggleClass() {
+    document.querySelector('.header-burger').classList.toggle('active');
+    document.querySelector('.header-menu').classList.toggle('active');
+}
+
+
+
+// jquery code
+
+// $(document).ready(function () {
+//     $('.header-burger').click(function (event) {
+//         $('.header-burger,.header-menu').toggleClass('active');
+//         $('body').toggleClass('lock');
+//     });
+// });
