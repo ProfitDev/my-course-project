45d286dede41741055bfdc7b2825519c618c1d91
ProfitDev <szhylyuk84@gmail.com>
2021-02-25 02:35:53 +0200

    css 1.1  update

--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     $('.slider').slick({
-        // arrows: true,
+        arrows: true,
         // adaptiveHeight: true,
         slidesToShow: 4,
         slidesToScroll: 3,
